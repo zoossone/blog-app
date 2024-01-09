a28b06ca1af73e3b6dba53f137f885a33201e3db
zoossone <zoossone@gmail.com>
2024-01-10 00:45:02 +0900

    list & css

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,11 +10,35 @@ export default function Home () {
                     <Link to="/profile">프로필</Link>
                 </div>
             </header>
-            <div className="post-list">Post Lists</div>
+            <div className="post__navigation">
+                <div className="post__navigation--active">전체</div>
+                <div>나의 글</div>
+            </div>
+            <div className="post__list">
+                {[...Array(10)].map((el, index) => (
+                    <div key={index} className="post__box">
+                        <Link to={`/posts/${index}`}>
+                            <div className="post__profile-box">
+                                <div className="post__profile"></div>
+                                <div className="post__author-name">누구누구</div>
+                                <div className="post__date">2024.01.10 수요일</div>
+                            </div>
+                            <div className="post__title">게시글 {index}</div>
+                            <div className="post__text">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium repellendus repudiandae consequuntur blanditiis sunt iste omnis quod. Obcaecati a corporis dolore. Accusamus deserunt placeat debitis officiis asperiores maiores, deleniti fugit?
+                            </div>
+                            <div className="post__utils-box">
+                                <div className="post__delete">삭제</div>
+                                <div className="post__edit">수정</div>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
+            </div>
             <footer>
-                <div>menu 1</div>
-                <div>menu 2</div>
-                <div>menu 3</div>
+                <Link to="/posts/new">글쓰기</Link>
+                <Link to="/posts">게시글</Link>
+                <Link to="/profile">프로필</Link>
             </footer>
         </div>
     )
