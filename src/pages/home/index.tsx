9745dbb6538a0cc45b56a06aad1ebc7a466367a7
zoossone <zoossone@gmail.com>
2024-01-12 01:19:45 +0900

    컴포넌트 절대경로, Carousel

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,13 @@
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
-import PostList from "../../components/PostList";
+import Header from "components/Header";
+import Footer from "components/Footer";
+import PostList from "components/PostList";
+import Carousel from "components/Carousel";
 
 export default function Home () {
     return (
         <>
             <Header />
+            <Carousel />
             <PostList />
             <Footer />
         </>
--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,7 @@
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
-import Profile from "../../components/Profile";
-import PostList from "../../components/PostList";
+import Footer from "components/Footer";
+import Header from "components/Header";
+import Profile from "components/Profile";
+import PostList from "components/PostList";
 
 export default function ProfilePage() {
     return (
